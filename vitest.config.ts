14a3cf8ac47b7d688a179288e124c854c187c777
Grigory <techuila.sunset@gmail.com>
2025-06-04 16:54:50 +0500

    test(vitest): marginal perf improvement

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: UserConfig = defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    pool: "threads",
+    isolate: false,
+  },
 });
 export default config;
